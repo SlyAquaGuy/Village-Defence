3821d849828fb2e8235a277a2d245c99d733546b
Max Padovan <122199194+max-padovan@users.noreply.github.com>
2023-01-26 21:59:08 +0930

    cube physics - max
    float size;
    //size float hehe

---
+++ Village Defence/Assets/CubeScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    float size;
+    //size float, hehe
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
